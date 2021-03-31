75a5404b62725bd7ac93ea4cef93df5482447b5d
Tiffany Vallo <tiffanyvallo@tiffanys-mbp.home>
2021-03-31 17:14:55 +0100

    Docked bike and bike docked added

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,4 +5,10 @@ describe DockingStation do
   it "gets a bike and expects the bike to be working" do
     expect(subject.release_bike).to be_working
   end
+  it { is_expected.to respond_to :dock}
+  it "checks bike is docked" do
+    bike = subject.release_bike
+    subject.dock(bike)
+    expect(subject.bike).to eq(bike)
+  end
 end
