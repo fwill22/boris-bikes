f9a3c51850cdfefc68adb56b16a66048ea5d0c28
jasonrowsell <jasonrowsell@hotmail.co.uk>
2021-03-31 17:38:57 +0100

    Stage 12: refactored RSpec

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,14 +1,19 @@
 require "docking_station"
 
 describe DockingStation do
+
   it { is_expected.to respond_to :release_bike }
+
   it "gets a bike and expects the bike to be working" do
     expect(subject.release_bike).to be_working
   end
+
   it { is_expected.to respond_to :dock}
+
   it "checks bike is docked" do
     bike = subject.release_bike
     subject.dock(bike)
     expect(subject.bike).to eq(bike)
   end
+  
 end
