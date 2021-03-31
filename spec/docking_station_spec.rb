7d3a7d04bf3231652d1ddcc0dcdc855a74d5f8cc
fwill22 <79596926+fwill22@users.noreply.github.com>
2021-03-31 14:29:56 +0100

    Created Bike spec and Bike class

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,6 +1,6 @@
 require "docking_station"
 
 describe DockingStation do
-  it { respond_to :release_bike } #release_bike needs to be symbol
+  it { is_expected.to respond_to :release_bike } #release_bike needs to be symbol
 end
 
