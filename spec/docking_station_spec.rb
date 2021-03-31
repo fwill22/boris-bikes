ce60d350ccc8ee21ddb4509db1af47f70f42a631
fwill22 <79596926+fwill22@users.noreply.github.com>
2021-03-31 15:24:08 +0100

    Built feature test for bike working method

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,6 +1,8 @@
 require "docking_station"
 
 describe DockingStation do
-  it { is_expected.to respond_to :release_bike } #release_bike needs to be symbol
+  it { is_expected.to respond_to :release_bike }
+  it "gets a bike and expects the bike to be working" do
+    expect(subject.release_bike).to be_working
+  end
 end
-
